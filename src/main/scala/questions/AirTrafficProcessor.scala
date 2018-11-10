5e490111c961c49b3afc08dee2706d79c9727470
cfleur <cfleur@users.noreply.github.com>
2018-11-10 16:49:15 +0200

    Implement correct weather delay method and did not fly method with issues: Did not fly works locally with lines 218-220 but fail test requirements from main at submission due to additional arguement in method

--- src/main/scala/questions/AirTrafficProcessor.scala
+++ src/main/scala/questions/AirTrafficProcessor.scala
@@ -2,7 +2,10 @@ package questions
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions
 import org.apache.spark.sql.expressions.Window
+
+import org.apache.spark.sql.functions.{min, max, avg}
 /** AirTrafficProcessor provides functionalites to
 * process air traffic data
 * Spark SQL tables available from start:
@@ -169,256 +172,275 @@ class AirTrafficProcessor(spark: SparkSession,
     * Columns FlightNum and Dest are included.
     */
     def cancelledDueToSecurity(df: DataFrame): DataFrame = {
-        val cancelledSecurity = spark
-          .sql("SELECT FlightNum, Dest, FROM airTraffic WHERE CancellationCode = 'D'")
-        return cancelledSecurity
+        return df.select("FlightNum", "Dest").where($"CancellationCode"==="D")
     }
 
-//    /** What was the longest weather delay between January
-//    * and march (1.1-31.3)?
-//    *
-//    * Example output:
-//    * +----+
-//    * | _c0|
-//    * +----+
-//    * |1148|
-//    * +----+
-//    *
-//    * @return DataFrame containing the highest delay which
-//    * was due to weather.
-//    */
-//    def longestWeatherDelay(df: DataFrame): DataFrame = {
+    /** What was the longest weather delay between January
+    * and march (1.1-31.3)?
+    *
+    * Example output:
+    * +----+
+    * | _c0|
+    * +----+
+    * |1148|
+    * +----+
+    *
+    * @return DataFrame containing the highest delay which
+    * was due to weather.
+    */
+    def longestWeatherDelay(df: DataFrame): DataFrame = {
+        return df.filter($"Month" > "0").filter($"Month" < "4").agg(max("WeatherDelay"))
+    }
+
+    /** Which airliners didn't fly.
+    * Table 'carriers' has this information.
+    *
+    * Example output:
+    * +--------------------+
+    * |         Description|
+    * +--------------------+
+    * |      Air Comet S.A.|
+    * |   Aerodynamics Inc.|
+    * |  Combs Airways Inc.|
+    * |   Evanston Aviation|
+    * |Lufthansa Cargo A...|
+    * +--------------------+
+    *
+    * @return airliner descriptions
+    */
+    def didNotFly(df: DataFrame): DataFrame = {
 //        ???
-//    }
-//
-//    /** Which airliners didn't fly.
-//    * Table 'carriers' has this information.
-//    *
-//    * Example output:
-//    * +--------------------+
-//    * |         Description|
-//    * +--------------------+
-//    * |      Air Comet S.A.|
-//    * |   Aerodynamics Inc.|
-//    * |  Combs Airways Inc.|
-//    * |   Evanston Aviation|
-//    * |Lufthansa Cargo A...|
-//    * +--------------------+
-//    *
-//    * @return airliner descriptions
-//    */
-//    def didNotFly(df: DataFrame): DataFrame = {
+//        return spark.sql("SELECT Description from carriers INNER JOIN (SELECT UniqueCarrier FROM airTraffic GROUP BY UniqueCarrier) ON Code <> UniqueCarrier")
+        val dft = spark.sql("SELECT DISTINCT UniqueCarriers FROM airTraffic")
+        return spark.sql("SELECT Description from carriers INNER JOIN dft ON carriers.Code = dft.UniqueCarriers")
 //        ???
-//    }
-//
-//    /** Find the airliners which travel
-//    * from Vegas to JFK. Sort them in descending
-//    * order by number of flights and append the
-//    * DataFrame with the data from carriers.csv.
-//    * Spark SQL table 'carriers' contains this data.
-//    * Vegas iasa code: LAS
-//    * JFK iasa code: JFK
-//    *
-//    *   Output should look like:
-//    *
-//    *   +--------------------+----+
-//    *   |         Description| Num|
-//    *   +--------------------+----+
-//    *   |     JetBlue Airways|1824|
-//    *   |Delta Air Lines Inc.|1343|
-//    *   |US Airways Inc. (...| 948|
-//    *   |American Airlines...| 366|
-//    *   +--------------------+----+
-//    *
-//    * @return DataFrame containing Columns Description
-//    * (airliner name) and Num(number of flights) sorted
-//    * in descending order.
-//    */
-//    def flightsFromVegasToJFK(df: DataFrame): DataFrame = {
-//        ???
-//    }
-//
-//    /** How much time airplanes spend on moving from
-//    * gate to the runway and vise versa at the airport on average.
-//    * This method should return a DataFrame containing this
-//    * information per airport.
-//    * Columns 'TaxiIn' and 'TaxiOut' tells time spend on taxiing.
-//    * Order by time spend on taxiing in ascending order. 'TaxiIn'
-//    * means time spend on taxiing in departure('Origin') airport
-//    * and 'TaxiOut' spend on taxiing in arrival('Dest') airport.
-//    * Column name's should be 'airport' for ISA airport code and
-//    * 'taxi' for average time spend taxiing
-//    *
-//    *
-//    * DataFrame should look like:
-//    *   +-------+-----------------+
-//    *   |airport|             taxi|
-//    *   +-------+-----------------+
-//    *   |    BRW|5.084736087380412|
-//    *   |    OME|5.961294471783976|
-//    *   |    OTZ|6.866595496262391|
-//    *   |    DAL|6.983973195822733|
-//    *   |    HRL|7.019248180512919|
-//    *   |    SCC|7.054629009320311|
-//    *   +-------+-----------------+
-//    *
-//    * @return DataFrame containing time spend on taxiing per
-//    * airport ordered in ascending order.
-//    */
-//    def timeSpentTaxiing(df: DataFrame): DataFrame = {
-//        ???
-//    }
-//
-//    /** What is the median travel distance?
-//    * Field Distance contains this information.
-//    *
-//    * Example output:
-//    * +-----+
-//    * |  _c0|
-//    * +-----+
-//    * |581.0|
-//    * +-----+
-//    *
-//    * @return DataFrame containing the median value
-//    */
-//    def distanceMedian(df: DataFrame): DataFrame = {
-//        ???
-//    }
-//
-//    /** What is the carrier delay, below which 95%
-//    * of the observations may be found?
-//    *
-//    * Example output:
-//    * +----+
-//    * | _c0|
-//    * +----+
-//    * |77.0|
-//    * +----+
-//    *
-//    * @return DataFrame containing the carrier delay
-//    */
-//    def score95(df: DataFrame): DataFrame = {
-//        ???
-//    }
-//
-//
-//    /** From which airport are flights cancelled the most?
-//    * What percentage of flights are cancelled from a specific
-//    * airport?
-//    * cancelledFlights combines flight data with
-//    * location data. Returns a DataFrame containing
-//    * columns 'airport' and 'city' from 'airports'
-//    * and the sum of number of cancelled flights divided
-//    * by the number of all flights (we get percentage)
-//    * from 'airtraffic' table. Name should be 'percentage'.
-//    * Lastly result should be ordered by 'percentage' and
-//    * secondly by 'airport' both in descending order.
-//    *
-//    *
-//    *
-//    *
-//    *   Output should look something like this:
-//    *   +--------------------+--------------------+--------------------+
-//    *   |             airport|                city|          percentage|
-//    *   +--------------------+--------------------+--------------------+
-//    *   |  Telluride Regional|           Telluride|   0.211340206185567|
-//    *   |  Waterloo Municipal|            Waterloo| 0.17027863777089783|
-//    *   |Houghton County M...|             Hancock| 0.12264150943396226|
-//    *   |                Adak|                Adak| 0.09803921568627451|
-//    *   |Aspen-Pitkin Co/S...|               Aspen| 0.09157716223855286|
-//    *   |      Sioux Gateway |          Sioux City| 0.09016393442622951|
-//    *   +--------------------+--------------------+--------------------+
-//    *
-//    * @return DataFrame containing columns airport, city and percentage
-//    */
-//    def cancelledFlights(df: DataFrame): DataFrame = {
-//        ???
-//    }
-//
-//    /**
-//    * Calculates the linear least squares approximation for relationship
-//    * between DepDelay and WeatherDelay.
-//    *  - First filter out entries where DepDelay < 0
-//    *  - there are definitely multiple data points for a single
-//    *    DepDelay value so calculate the average WeatherDelay per
-//    *    DepDelay
-//    *  - Calculate the linear least squares (c+bx=y) where
-//    *    x equals DepDelay and y WeatherDelay. c is the constant term
-//    *    and b is the slope.
-//    *
-//    *
-//    * @return tuple, which has the constant term first and the slope second
-//    */
-//    def leastSquares(df: DataFrame):(Double, Double) = {
-//        ???
-//    }
-//
-//    /**
-//    * Calculates the running average for DepDelay per day.
-//    * Average should be taken from both sides of the day we
-//    * are calculating the running average. You should take
-//    * 5 days from both sides of the day you are counting the running
-//    * average into consideration. For instance
-//    * if we want to calculate the average for the 10th of
-//    * January we have to take days: 5,6,7,8,9,10,11,12,13,14 and
-//    * 15 into account.
-//    *
-//    * Complete example
-//    * Let's assume that data looks like this
-//    *
-//    * +----+-----+---+--------+
-//    * |Year|Month|Day|DepDelay|
-//    * +----+-----+---+--------+
-//    * |2008|    3| 27|      12|
-//    * |2008|    3| 27|      -2|
-//    * |2008|    3| 28|       3|
-//    * |2008|    3| 29|      -5|
-//    * |2008|    3| 29|      12|
-//    * |2008|    3| 30|       5|
-//    * |2008|    3| 31|      47|
-//    * |2008|    4|  1|      45|
-//    * |2008|    4|  1|       2|
-//    * |2008|    4|  2|      -6|
-//    * |2008|    4|  3|       0|
-//    * |2008|    4|  3|       4|
-//    * |2008|    4|  3|      -2|
-//    * |2008|    4|  4|       2|
-//    * |2008|    4|  5|      27|
-//    * +----+-----+---+--------+
-//    *
-//    *
-//    * When running average is calculated
-//    * +----------+------------------+
-//    * |      date|    moving_average|
-//    * +----------+------------------+
-//    * |2008-03-27|13.222222222222221|
-//    * |2008-03-28|              11.3|
-//    * |2008-03-29| 8.846153846153847|
-//    * |2008-03-30| 8.357142857142858|
-//    * |2008-03-31|               9.6|
-//    * |2008-04-01|               9.6|
-//    * |2008-04-02|10.307692307692308|
-//    * |2008-04-03|10.916666666666666|
-//    * |2008-04-04|              12.4|
-//    * |2008-04-05|13.222222222222221|
-//    * +----------+------------------+
-//    *
-//    * For instance running_average for
-//    * date 2008-04-03 => (-5+12+5+47+45+2-6+0+4-2+2+27)/12
-//    * =10.916666
-//    *
-//    *
-//    * @return DataFrame with schema 'date' (YYYY-MM-DD) (string type) and 'moving_average' (double type)
-//    * ordered by 'date'
-//    */
-//    def runningAverage(df: DataFrame): DataFrame = {
-//        ???
-//    }
+//        Following 3 lines work but tests on assignment put constraints on main that do not allow for function call to take 2 args.
+//        val cr = createOrReplaceTempView(carriers)
+//        val tempdf = df.groupBy("UniqueCarrier").count()
+//        val tempcr = tempdf.select("UniqueCarrier").join(cr, tempdf("UniqueCarrier") === cr("Code"), "inner")
+
+//        people.filter("age > 30")
+//          .join(department, people("deptId") === department("id"))
+//          .groupBy(department("name"), "gender")
+//          .agg(avg(people("salary")), max(people("age")))
+//        return temp.join(carriers("description"), temp("UniqueCarrier") === carriers("Code")))
+//        return tempcr.select("Description")
+//        val dft.registerTempTable("df")
+//        val carrierst.registerTempTable("carriers")
+//        return sqlContext.sql("SELECT description FROM carriers INNER JOIN (SELECT DISTINCT UniqueCarriers FROM df) ON Code <> UniqueCarrier")
+//        return carriers.schema
+//        return carriers.select("Description").join(carriers, df("UniqueCarrier").groupBy("UniqueCarrier") === carriers("Code"))
+    }
+
+    /** Find the airliners which travel
+    * from Vegas to JFK. Sort them in descending
+    * order by number of flights and append the
+    * DataFrame with the data from carriers.csv.
+    * Spark SQL table 'carriers' contains this data.
+    * Vegas iasa code: LAS
+    * JFK iasa code: JFK
+    *
+    *   Output should look like:
+    *
+    *   +--------------------+----+
+    *   |         Description| Num|
+    *   +--------------------+----+
+    *   |     JetBlue Airways|1824|
+    *   |Delta Air Lines Inc.|1343|
+    *   |US Airways Inc. (...| 948|
+    *   |American Airlines...| 366|
+    *   +--------------------+----+
+    *
+    * @return DataFrame containing Columns Description
+    * (airliner name) and Num(number of flights) sorted
+    * in descending order.
+    */
+    def flightsFromVegasToJFK(df: DataFrame): DataFrame = {
+        ???
+//        val tempdf = df.groupBy("UniqueCarrier").count()
+    }
+
+    /** How much time airplanes spend on moving from
+    * gate to the runway and vise versa at the airport on average.
+    * This method should return a DataFrame containing this
+    * information per airport.
+    * Columns 'TaxiIn' and 'TaxiOut' tells time spend on taxiing.
+    * Order by time spend on taxiing in ascending order. 'TaxiIn'
+    * means time spend on taxiing in departure('Origin') airport
+    * and 'TaxiOut' spend on taxiing in arrival('Dest') airport.
+    * Column name's should be 'airport' for ISA airport code and
+    * 'taxi' for average time spend taxiing
+    *
+    *
+    * DataFrame should look like:
+    *   +-------+-----------------+
+    *   |airport|             taxi|
+    *   +-------+-----------------+
+    *   |    BRW|5.084736087380412|
+    *   |    OME|5.961294471783976|
+    *   |    OTZ|6.866595496262391|
+    *   |    DAL|6.983973195822733|
+    *   |    HRL|7.019248180512919|
+    *   |    SCC|7.054629009320311|
+    *   +-------+-----------------+
+    *
+    * @return DataFrame containing time spend on taxiing per
+    * airport ordered in ascending order.
+    */
+    def timeSpentTaxiing(df: DataFrame): DataFrame = {
+        ???
+    }
+
+    /** What is the median travel distance?
+    * Field Distance contains this information.
+    *
+    * Example output:
+    * +-----+
+    * |  _c0|
+    * +-----+
+    * |581.0|
+    * +-----+
+    *
+    * @return DataFrame containing the median value
+    */
+    def distanceMedian(df: DataFrame): DataFrame = {
+        ???
+    }
+
+    /** What is the carrier delay, below which 95%
+    * of the observations may be found?
+    *
+    * Example output:
+    * +----+
+    * | _c0|
+    * +----+
+    * |77.0|
+    * +----+
+    *
+    * @return DataFrame containing the carrier delay
+    */
+    def score95(df: DataFrame): DataFrame = {
+        ???
+    }
+
+
+    /** From which airport are flights cancelled the most?
+    * What percentage of flights are cancelled from a specific
+    * airport?
+    * cancelledFlights combines flight data with
+    * location data. Returns a DataFrame containing
+    * columns 'airport' and 'city' from 'airports'
+    * and the sum of number of cancelled flights divided
+    * by the number of all flights (we get percentage)
+    * from 'airtraffic' table. Name should be 'percentage'.
+    * Lastly result should be ordered by 'percentage' and
+    * secondly by 'airport' both in descending order.
+    *
+    *
+    *
+    *
+    *   Output should look something like this:
+    *   +--------------------+--------------------+--------------------+
+    *   |             airport|                city|          percentage|
+    *   +--------------------+--------------------+--------------------+
+    *   |  Telluride Regional|           Telluride|   0.211340206185567|
+    *   |  Waterloo Municipal|            Waterloo| 0.17027863777089783|
+    *   |Houghton County M...|             Hancock| 0.12264150943396226|
+    *   |                Adak|                Adak| 0.09803921568627451|
+    *   |Aspen-Pitkin Co/S...|               Aspen| 0.09157716223855286|
+    *   |      Sioux Gateway |          Sioux City| 0.09016393442622951|
+    *   +--------------------+--------------------+--------------------+
+    *
+    * @return DataFrame containing columns airport, city and percentage
+    */
+    def cancelledFlights(df: DataFrame): DataFrame = {
+        ???
+    }
+
+    /**
+    * Calculates the linear least squares approximation for relationship
+    * between DepDelay and WeatherDelay.
+    *  - First filter out entries where DepDelay < 0
+    *  - there are definitely multiple data points for a single
+    *    DepDelay value so calculate the average WeatherDelay per
+    *    DepDelay
+    *  - Calculate the linear least squares (c+bx=y) where
+    *    x equals DepDelay and y WeatherDelay. c is the constant term
+    *    and b is the slope.
+    *
+    *
+    * @return tuple, which has the constant term first and the slope second
+    */
+    def leastSquares(df: DataFrame):(Double, Double) = {
+        ???
+    }
+
+    /**
+    * Calculates the running average for DepDelay per day.
+    * Average should be taken from both sides of the day we
+    * are calculating the running average. You should take
+    * 5 days from both sides of the day you are counting the running
+    * average into consideration. For instance
+    * if we want to calculate the average for the 10th of
+    * January we have to take days: 5,6,7,8,9,10,11,12,13,14 and
+    * 15 into account.
+    *
+    * Complete example
+    * Let's assume that data looks like this
+    *
+    * +----+-----+---+--------+
+    * |Year|Month|Day|DepDelay|
+    * +----+-----+---+--------+
+    * |2008|    3| 27|      12|
+    * |2008|    3| 27|      -2|
+    * |2008|    3| 28|       3|
+    * |2008|    3| 29|      -5|
+    * |2008|    3| 29|      12|
+    * |2008|    3| 30|       5|
+    * |2008|    3| 31|      47|
+    * |2008|    4|  1|      45|
+    * |2008|    4|  1|       2|
+    * |2008|    4|  2|      -6|
+    * |2008|    4|  3|       0|
+    * |2008|    4|  3|       4|
+    * |2008|    4|  3|      -2|
+    * |2008|    4|  4|       2|
+    * |2008|    4|  5|      27|
+    * +----+-----+---+--------+
+    *
+    *
+    * When running average is calculated
+    * +----------+------------------+
+    * |      date|    moving_average|
+    * +----------+------------------+
+    * |2008-03-27|13.222222222222221|
+    * |2008-03-28|              11.3|
+    * |2008-03-29| 8.846153846153847|
+    * |2008-03-30| 8.357142857142858|
+    * |2008-03-31|               9.6|
+    * |2008-04-01|               9.6|
+    * |2008-04-02|10.307692307692308|
+    * |2008-04-03|10.916666666666666|
+    * |2008-04-04|              12.4|
+    * |2008-04-05|13.222222222222221|
+    * +----------+------------------+
+    *
+    * For instance running_average for
+    * date 2008-04-03 => (-5+12+5+47+45+2-6+0+4-2+2+27)/12
+    * =10.916666
+    *
+    *
+    * @return DataFrame with schema 'date' (YYYY-MM-DD) (string type) and 'moving_average' (double type)
+    * ordered by 'date'
+    */
+    def runningAverage(df: DataFrame): DataFrame = {
+        ???
+    }
 }
 /**
 *
 *  Change the student id
 */
 object AirTrafficProcessor {
-    val studentId = "XXXXXX"
+    val studentId = "718114"
 }
